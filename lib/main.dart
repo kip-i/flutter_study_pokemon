71d8dbc40e2fe1ce43c9834909642ea2972ad973
kip-i <kppisj@gmail.com>
2023-11-12 15:48:29 +0900

    feat: Stackで重ねる

--- lib/main.dart
+++ lib/main.dart
@@ -27,20 +27,37 @@ class MyHomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children:[
-            Image.network(
-              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-              height: 100,
-              width: 100,
+            Stack(
+              children:[
+                Container(
+                  padding: const EdgeInsets.all(32),
+                  child: Image.network(
+                      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+                      height: 100,
+                      width: 100,
+                  )
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: const Text(
+                    'No.25',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          const Text(
-            'pikachu', 
-            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-          ),
-          const Chip(
-            label: Text('Electric'),
-            backgroundColor: Colors.yellow,
-          )
-        ])
+            const Text(
+              'pikachu',
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            ),
+            const Chip(
+              backgroundColor: Colors.yellow,
+              label: Text('electric'),
+            ),
+          ])
       ),
     );
   }
