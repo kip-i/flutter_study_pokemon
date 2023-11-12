29828e4582c19b89eaacca1b184eb7230e9c70e0
kip-i <kppisj@gmail.com>
2023-11-12 17:39:10 +0900

    feat: StatelessWidget->StatefulWidgetにする

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,12 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class TopPage extends StatelessWidget {
+class TopPage extends StatefulWidget {
   const TopPage({Key? key}) : super(key: key);
+  @override
+  _TopPageState createState() => _TopPageState();
+}
+class _TopPageState extends State<TopPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
