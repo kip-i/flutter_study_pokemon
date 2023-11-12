a0bb9cda7c9ecd58e1685a39aa3978b784b8c60a
kip-i <kppisj@gmail.com>
2023-11-12 14:52:42 +0900

    feat: ピカチュウ表示

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,11 @@ class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Hello World', style: TextStyle(fontSize: 36)),
+        child: Image.network(
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"
+          ),
       ),
     );
   }
