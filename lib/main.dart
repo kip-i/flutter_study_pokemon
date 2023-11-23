fb87ad224e646253ee72f88dfc9daf7322af6b80
kip-i <kppisj@gmail.com>
2023-11-24 01:05:06 +0900

    feat: ページ遷移で選択

--- lib/main.dart
+++ lib/main.dart
@@ -74,12 +74,57 @@ class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: const [
+      children: [
         ListTile(
-          leading: Icon(Icons.lightbulb),
-          title: Text('Dark/Light Mode'),
+          leading: const Icon(Icons.lightbulb),
+          title: const Text('Dark/Light Mode'),
+          onTap: () => {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => const ThemeModeSelectionPage(),
+            )),
+          },
         ),
       ],
+    );
+  }
+}
+
+class ThemeModeSelectionPage extends StatelessWidget {
+  const ThemeModeSelectionPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            ListTile(
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.system,
+              groupValue: ThemeMode.system,
+              title: const Text('System'),
+              onChanged: (val) => {},
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.dark,
+              groupValue: ThemeMode.system,
+              title: const Text('Dark'),
+              onChanged: (val) => {},
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.light,
+              groupValue: ThemeMode.system,
+              title: const Text('Light'),
+              onChanged: (val) => {},
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
