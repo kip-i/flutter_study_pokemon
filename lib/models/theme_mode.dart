a16428e81a36694d55a8fe22af61a6ef03379220
kip-i <kppisj@gmail.com>
2023-11-24 04:09:07 +0900

    feat: prefを追加(SharedPreferences)

--- lib/models/theme_mode.dart
+++ lib/models/theme_mode.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../utils/theme_mode.dart';
 
 class ThemeModeNotifier extends ChangeNotifier {
   late ThemeMode _themeMode;
 
-  ThemeModeNotifier() {
-    _init();
+  ThemeModeNotifier(SharedPreferences pref) {
+    _init(pref);
   }
 
   ThemeMode get mode => _themeMode;
 
-  void _init() async {
-    _themeMode = await loadThemeMode();
+  void _init(SharedPreferences pref) async {
+    _themeMode = await loadThemeMode(pref);
     notifyListeners();
   }
 
